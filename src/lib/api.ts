c88e1af27789bb8737b3a254f3c2594ce691b5d5
carellihoula <carellihoula@gmail.com>
2025-03-02 13:46:04 +0100

    feature: update fetchUserSensors to use NEXT_PUBLIC_AUTH_API_URL for API calls

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 export const fetchUserSensors = async (userId: string) => {
   try {
     const response = await fetch(
-      `${process.env.AUTH_API_URL}/sensors/user/${userId}`
+      `${process.env.NEXT_PUBLIC_AUTH_API_URL}/sensors/user/${userId}`
     );
     // Vérifie si la réponse est du JSON
     const text = await response.text();
