74a560d234b3946d16e1e5b3cf40ee05cd07342c
carellihoula <81572326+carellihoula@users.noreply.github.com>
2025-03-05 02:51:11 +0000

    refacto auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function page() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen">
       <TabsDemo />
     </div>
   );
